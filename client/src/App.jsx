46bfc1d2e0d3d8cf3723342327bfd616955b324f
sayam2420 <sprof.242@gmail.com>
2025-07-07 01:26:52 +0530

    corrected some mistakes in app.jsx and also renamed few folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,15 @@
 
 import './App.css'
-import Login from "./Login.jsx";
+import Create1 from "./Create1.jsx";
 
 function App() {
 
   return (
     
-      <div style={{width:"100%"}}>
-        <div style={{display:"flex", width:"100%", height:"100vh"}}>
-          <div style={{width:"40%",
-            height:"100%",
-            backgroundImage:`url("https://www.globalfleet.com/sites/default/files/field/image/shutterstock_1019141671.jpg")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",  /* Center the image */
-            backgroundRepeat: "noRepeat"/* Prevent tiling */
-          }}>
-          </div>
-          <div style={{display:"flex",justifyContent:"center", width:"60%", height:"100%", padding:"0%", margin:"0%"}}>
-          <Login/>
-          </div>
-        </div>
+      <div style={{width:"100%", height:"100vh"}}>
+        
+          <Create1/>
+          
       </div>
 
   )
---
+++ client/src/Create1.jsx
@@ -0,0 +1,53 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Create1.css"
+import { useState } from "react";
+export default function Login(){
+    const [userType, setUserType] = useState("");
+    return (
+        <div style={{display:"flex", width:"100%", height:"100vh"}}>
+            <div style={{width:"40%",
+                height:"100%",
+                backgroundImage:`url("https://www.globalfleet.com/sites/default/files/field/image/shutterstock_1019141671.jpg")`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",  /* Center the image */
+                backgroundRepeat: "noRepeat"/* Prevent tiling */
+            }}>
+            </div>
+        
+            <div className='Login' >
+                <form style={{width:"50%",display:"flex",flexDirection:"column",justifyContent:"center",alignItems :"center",gap:"20px"}} action="#">
+                    <h2>Create your account</h2>
+                    <div className="form-floating loginDivs">
+                        <select
+                        className="form-select loginInputs"
+                        style={{textAlignLast: "left" }}
+                        value={userType}
+                        onChange={(e) => setUserType(e.target.value)}
+                        id="floatingSelect" aria-label="Floating label select example">
+                            <option value="" disabled hidden>
+                                
+                            </option>
+                            <option value="hospital">HOSPITAL</option>
+                            <option value="admin">ADMIN</option>
+                        </select>
+                        <label style={{fontSize:"15px",paddingTop:"12px" }} htmlFor="floatingSelect">USER TYPE</label>
+                    </div>
+                    <div className="form-floating loginDivs">
+                        <input type='text' style={{ textAlign: "left" }} className="form-control loginInputs" id="floatingTextareaname"></input>
+                        <label style={{fontSize:"12px", paddingTop:"2px"}} htmlFor="floatingTextareaname">EMAIL OR USERNAME</label>
+                    </div>
+                    <div className="form-floating loginDivs">
+                        <input type='password' style={{ textAlign: "left" }} className="form-control loginInputs" id="floatingTextareapwd"></input>
+                        <label style={{fontSize:"12px", paddingTop:"2px"}} htmlFor="floatingTextareapwd">PASSSWORD</label>
+                    </div>
+                    <div className="form-button loginDivs">
+                        <button className="btn btn-danger loginButton" type="button">NEXT <span style={{ marginLeft: '8px' }}>➝</span>
+                        </button>
+                    </div>
+                    <p>Already Have an account?<a className='text-danger' href="#">Login</a></p>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
