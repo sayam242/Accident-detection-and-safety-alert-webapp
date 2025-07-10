1ba600e30cb0e7272741e3a9f97e89cf34cf24ca
Suminder <sumi786nyol@gmail.com>
2025-07-10 13:42:21 +0530

    Add Leaflet and React-Leaflet for map functionality; update MapPicker component

--- client/src/Components/MapPicker.jsx
+++ client/src/Components/MapPicker.jsx
@@ -1,29 +1,48 @@
+
 import React, { useState, useCallback } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-const containerStyle = { width: "100%", height: "400px" };
-const center = { lat: 28.6139, lng: 77.2090 };
 
-function MapPicker({ onLocationSelect, onCancel }) {
-  const [marker, setMarker] = useState(null);
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+});
 
-  const handleMapClick = useCallback(
-    (event) => {
-      const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-      setMarker(location);
-      onLocationSelect(location);
-    },
-    [onLocationSelect]
-  );
+const MapPicker = ({ onLocationSelect, onCancel }) => {
+  const [position, setPosition] = useState(null);
+
+
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click(e) {
+        const location = { lat: e.latlng.lat, lng: e.latlng.lng };
+        setPosition(location); 
+        if (onLocationSelect) onLocationSelect(location);
+      },
+    });
+
+    return position ? <Marker position={position} /> : null;
+  };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCHvXvJqzUred2A7c6EE5mewhpjQTWvTJM">
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onClick={handleMapClick}>
-        {marker && <Marker position={marker} />}
-      </GoogleMap>
+    <div>
+      <MapContainer
+        center={[28.6139, 77.2090]} 
+        zoom={13}
+        style={{ height: "400px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <LocationMarker />
+      </MapContainer>
       <button onClick={onCancel}>Cancel</button>
-    </LoadScript>
+    </div>
   );
-}
+};
 
 export default MapPicker;
