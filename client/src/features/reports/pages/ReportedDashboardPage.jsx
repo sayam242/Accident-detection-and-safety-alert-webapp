2bde2b329937c965bd400a4a235b2fed85fd2d12
Sayam <sayam2422004@gmail.com>
2026-05-21 15:35:23 +0530

    fix: correct import path for DetailsPopup component

--- client/src/features/reports/pages/ReportedDashboardPage.jsx
+++ client/src/features/reports/pages/ReportedDashboardPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import DetailsPopup from "../Components/map/DetailsPopup";
+import DetailsPopup from "../components/map/DetailsPopup";
 import ReportedTable from "../components/ReportedTable";
 import { useReports } from "../hooks/useReports";
 import { socket } from "../../../socket";
